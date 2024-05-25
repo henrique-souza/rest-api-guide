191f17ee109a0379f1cbe20b0b10e78009842657
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2024-05-25 13:10:16 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :facts
+      end
+    end
+  end
+end
